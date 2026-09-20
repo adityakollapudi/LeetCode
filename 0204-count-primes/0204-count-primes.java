ce7585fc6f000731b421aea1d10cac71f3df20d5
adityakollapudi <adityakollapudi@gmail.com>
2026-09-20 20:29:06 +0530

    Time: 103 ms (79.97%), Space: 51.4 MB (63.26%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] check=new boolean[n];
+        for(int i=0;i<n;i++){
+           check[i]=true;
+        }
+        // check[0]=false;
+        // check[1]=false;
+        for(int i=2;i*i<n;i++){
+            if(check[i]){
+                for(int j=i*i;j<n;j+=i){
+                    check[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(check[i]){
+                count+=1;
+            }
+        }
+
+        return count;
+    }
+}
